c0b7c4f419ac86d7a7d004ec3e04111f1900a3c0
Ed Doddridge <ewd@mit.edu>
2019-01-30 21:26:21 -0500

    Refactor into function

--- camera.py
+++ camera.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-print(" ")
-print("**************************************************")
-print("*   Running timelapse script in the background   *")
-
 from cam_utils import working_directory, make_dir
 from picamera import PiCamera
 from time import sleep
@@ -11,33 +7,42 @@ import datetime
 import os
 import glob
 
-now = datetime.datetime.now()
-now_string = now.strftime("%Y-%m-%d-%H%M")
+def run_timelapse():
+    """Start taking a timelapse."""
 
+    print(" ")
+    print("**************************************************")
+    print("*   Running timelapse script in the background   *")
 
-print("*    timelapse started at {0}        *".format(now_string))
-print("**************************************************")
-print(" ")
+    now = datetime.datetime.now()
+    now_string = now.strftime("%Y-%m-%d-%H%M")
 
-path = '/home/pi/Documents/bike-lapse/'
+    print("*    timelapse started at {0}        *".format(now_string))
+    print("**************************************************")
+    print(" ")
 
-picture_dir = path + now_string
-# use iterative function to avoid duplicate diectories
-picture_dir = make_dir(picture_dir)
+    path = '/home/pi/Documents/bike-lapse/'
 
-camera = PiCamera()
-# 2x2 binned (2 megapixels)
-camera.resolution = (1640, 1232)
-# full resolution (8 megapixels)
-#camera.resolution = (3280, 2464)
-camera.rotation = (270)
-camera.meter_mode = 'matrix'
-# minimum time between images in seconds
-delay = 0.25
+    picture_dir = path + now_string
+    # use iterative function to avoid duplicate diectories
+    # this is important if the RPi doesn't keep time properly
+    picture_dir = make_dir(picture_dir)
 
-with working_directory(picture_dir):
-    for i, filename in enumerate(camera.capture_continuous('image{counter:06d}.jpg')):
-        sleep(delay)
-#        if i == 2:
-#            break
+    camera = PiCamera()
+    # 2x2 binned (2 megapixels)
+    camera.resolution = (1640, 1232)
+    # full resolution (8 megapixels)
+    #camera.resolution = (3280, 2464)
+    camera.rotation = (270)
+    camera.meter_mode = 'matrix'
+    # minimum time between images in seconds
+    delay = 0.25
 
+    with working_directory(picture_dir):
+        for i, filename in enumerate(camera.capture_continuous('image{counter:06d}.jpg')):
+            sleep(delay)
+    #        if i == 2:
+    #            break
+
+if __name__ == '__main__':
+    run_timelapse()
